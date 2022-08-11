bc4d7fb39eeae9e44561b07949f0e46c7838314e
andrey-korchevniy <91483336+andrey-korchevniy@users.noreply.github.com>
2022-08-11 13:55:43 +0300

    Update calculateTeamFinanceReport.js

--- src/js/calculateTeamFinanceReport.js
+++ src/js/calculateTeamFinanceReport.js
@@ -13,7 +13,7 @@ export const calculateTeamFinanceReport = (team, salaries) => {
         if (salaries[category]) {
             const { salary, tax } = salaries[category];
             teamSpecialists[category] = Math.round(teamSpecialists[category] * salary * (1 / (1 - (tax.slice(0, -1) / 100))));
-            // 3. Creating report Object
+    // 3. Creating report Object
             report.totalBudgetTeam += teamSpecialists[category];
             report["totalBudget" + category] = teamSpecialists[category];
         }
